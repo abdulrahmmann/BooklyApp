ecdb5a7653fd93cba1641d009e2a2ad3613e88b1
Abdulrahman <abd3lra7man.078@gmail.com>
2023-07-25 22:31:18 +0300

    create featured_list_view.dart.

--- lib/features/home/presentation/views/widgets/custom_list_view_item.dart
+++ lib/features/home/presentation/views/widgets/custom_list_view_item.dart
@@ -6,15 +6,12 @@ class CustomListBooksItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height*.3,
-      child: AspectRatio(
-        aspectRatio: 2.5/4,
-        child: Container(
-          decoration: BoxDecoration(
-            image: const DecorationImage(image: AssetImage('assets/images/Book 1 Hightligh.png'), fit: BoxFit.fill),
-            borderRadius: BorderRadius.circular(16),
-          ),
+    return AspectRatio(
+      aspectRatio: 2.6/4,
+      child: Container(
+        decoration: BoxDecoration(
+          image: const DecorationImage(image: AssetImage('assets/images/book.png'), fit: BoxFit.fill),
+          borderRadius: BorderRadius.circular(16),
         ),
       ),
     );
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:booklyapp/core/utils/assets.dart';
 import 'package:booklyapp/features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:booklyapp/features/home/presentation/views/widgets/custom_list_view_item.dart';
+import 'package:booklyapp/features/home/presentation/views/widgets/featured_list_view.dart';
 import 'package:booklyapp/features/home/presentation/views/widgets/home_view_body.dart';
 import 'package:flutter/material.dart';
 
@@ -21,8 +22,7 @@ class HomeViewBody extends StatelessWidget {
           SizedBox(height: 42),
 
           // list of books -> ListView Horizontally.
-          CustomListBooksItem(),
-          SizedBox(height: 42),
+          FeaturedListView(),
         ],
       ),
     );
@@ -30,17 +30,6 @@ class HomeViewBody extends StatelessWidget {
 }
 
 
-class CustomListView extends StatelessWidget {
-  const CustomListView({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder (
-      scrollDirection: Axis.horizontal,
-        itemBuilder: (context, i) => const CustomListBooksItem()
-    );
-  }
-}
 
 
 
