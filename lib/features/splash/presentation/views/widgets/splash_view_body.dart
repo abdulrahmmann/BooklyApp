d985ceb47fa47e2e3c7f1544eb86f41bd7ec0c4d
Abdulrahman <abd3lra7man.078@gmail.com>
2023-07-25 20:18:53 +0300

    add sliding animation for text widgets in splash_view.dart.

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,8 +1,38 @@
 import 'package:booklyapp/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 
-class SplashViewBody extends StatelessWidget {
-  const SplashViewBody({super.key});
+class SplashViewBody extends StatefulWidget{
+  SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+
+}
+
+class _SplashViewBodyState extends State<SplashViewBody> with SingleTickerProviderStateMixin{
+
+  late AnimationController animationController;
+  late Animation<Offset> animation;
+
+  @override
+  void initState() {
+    super.initState();
+    createSlidingAnimation();
+  }
+  // create animation for text widget.
+  void createSlidingAnimation() {
+    animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 1));
+    animation = Tween<Offset>(begin: const Offset(0, 13), end: Offset.zero)
+        .animate(animationController);
+    animationController.forward();
+  }
+  // dispose from animation controller after worked.
+  @override
+  void dispose() {
+    super.dispose();
+    animationController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,12 +41,34 @@ class SplashViewBody extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         Image.asset(AssetsData.logo),
-        const Text(
-          'Read Books For free',
-          style: TextStyle(fontSize: 22),
-          textAlign: TextAlign.center,
-        ),
+        SlidingText(animation: animation),
       ],
+    );
+  }
+}
+
+class SlidingText extends StatelessWidget {
+  const SlidingText({
+    super.key,
+    required this.animation,
+  });
+
+  final Animation<Offset> animation;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: animation,
+      builder: (context, _) {
+        return SlideTransition(
+          position: animation,
+          child: const Text(
+            'Read Books For free',
+            style: TextStyle(fontSize: 22),
+            textAlign: TextAlign.center,
+          ),
+        );
+      }
     );
   }
 }
