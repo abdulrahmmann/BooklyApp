db6c63cd79e9444d7796ebb5d34f3b953cedc307
Abdulrahman <abd3lra7man.078@gmail.com>
2023-07-27 20:55:05 +0300

    create home repository, methods and create the failure file.

---
+++ lib/features/home/data/repository/home_repository.dart
@@ -0,0 +1,11 @@
+
+import 'package:booklyapp/core/errors/failures.dart';
+import 'package:booklyapp/features/home/data/model/BookModel.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class HomeRepository {
+
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks();
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks();
+
+}
