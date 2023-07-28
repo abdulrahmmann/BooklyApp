1ad506a97b0e471838989c67d1bbde8589ae3018
Abdulrahman <abd3lra7man.078@gmail.com>
2023-07-28 23:01:58 +0300

    create newest books cubit state.

---
+++ lib/features/home/data/manager/newest_books_cubit/newest_book_cubit.dart
@@ -0,0 +1,23 @@
+import 'package:bloc/bloc.dart';
+import 'package:booklyapp/features/home/data/model/BookModel.dart';
+import 'package:booklyapp/features/home/data/repository/home_repository.dart';
+import 'package:meta/meta.dart';
+
+part 'newest_book_state.dart';
+
+class NewestBookCubit extends Cubit<NewestBookState> {
+  NewestBookCubit(this.homeRepository) : super(NewestBookInitial());
+
+  // create methods for fetch newest books :
+  final HomeRepository homeRepository;
+  Future<void> fetchNewestBooks() async {
+    emit(NewestBookLoading());
+    var result = await homeRepository.fetchNewestBooks();
+
+    result.fold(
+            (Failure) => emit(NewestBookFailure(Failure.errorMessage)),
+            (books) => emit(NewestBookSuccess(books))
+    );
+  }
+
+}
