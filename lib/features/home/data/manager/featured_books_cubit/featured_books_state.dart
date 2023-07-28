c921f917f79ac8f77beffaaa954299a24650131e
Abdulrahman <abd3lra7man.078@gmail.com>
2023-07-28 22:41:04 +0300

    create featured books cubit state.

---
+++ lib/features/home/data/manager/featured_books_cubit/featured_books_state.dart
@@ -0,0 +1,22 @@
+part of 'featured_books_cubit.dart';
+
+
+abstract class FeaturedBooksState {}
+
+class FeaturedBooksInitial extends FeaturedBooksState {}
+
+class FeaturedBooksLoading extends FeaturedBooksState {}
+
+class FeaturedBooksFailure extends FeaturedBooksState {
+  final String errorMessage;
+
+  FeaturedBooksFailure(this.errorMessage);
+}
+
+class FeaturedBooksSuccess extends FeaturedBooksState {
+  final List<BookModel> books;
+
+  FeaturedBooksSuccess(this.books);
+
+}
+
